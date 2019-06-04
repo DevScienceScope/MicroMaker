0b6b88715748fe8f667048b5d38283171be5c4b6
DevScienceScope <developer@sciencescope.uk>
2019-06-04 14:28:00 +0100

    Add files via upload

---
+++ SSD1306_OLED.cpp
@@ -0,0 +1,70 @@
+#include "pxt.h"
+#include "Adafruit_SSD1306.h"
+
+using namespace pxt;
+
+namespace OLED {
+	#define SSD1306_ADDRESS 0x78
+	#undef printf
+
+
+	MicroBitI2C i2c(I2C_SDA0, I2C_SCL0);
+	Adafruit_SSD1306_I2c *oled;
+
+	void init(int height, int width, int orentation){
+		if (oled != NULL) delete oled;
+		oled = new Adafruit_SSD1306_I2c(i2c, SSD1306_ADDRESS, height, width, orentation);
+		oled->splash();
+		oled->display();
+	}
+	
+	//%
+	void init_terminal(int height, int width, int orentation){
+		if (oled != NULL) delete oled;
+		oled = new Adafruit_SSD1306_I2c(i2c, SSD1306_ADDRESS, height, width, orentation);
+		oled->clearDisplay();
+		oled->display();
+		oled->setTextCursor(0, 0);
+	}
+	
+
+	//%
+    void showStringNoNewLine(String text) {
+		oled->printf("%s", PXT_BUFFER_DATA(text));
+		oled->display();
+    }
+
+	//%
+    void showStringWithNewLine(String text) {
+		oled->printf("%s\n", PXT_BUFFER_DATA(text));
+		oled->display();
+    }
+	
+    //%
+    void showNumberWithoutNewLine (int number) {
+		oled->printf("%d", number);
+		oled->display();
+	}
+	
+	//%
+    void showNumberWithNewLine (int number) {
+		oled->printf("%d\n", number);
+		oled->display();
+	}
+	
+    //%
+    void NextLine () {
+		oled->printf("\n");
+		oled->display();
+	}
+
+	//%
+	void clearDisplay(){
+		oled->setTextCursor(0, 0);
+		oled->clearDisplay();
+		oled->display();
+	}
+
+    #define printf(...) uBit.serial.printf(__VA_ARGS__)
+
+}
